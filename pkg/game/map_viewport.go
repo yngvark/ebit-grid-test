8ab773b3fbd135a6657a3a0836270e82b763a41a
Yngvar Kristiansen <562343+yngvark@users.noreply.github.com>
2024-03-31 18:16:00 +0200

    chore: Test how ebitengine works

--- pkg/game/map_viewport.go
+++ pkg/game/map_viewport.go
@@ -1,26 +1,25 @@
 package game
 
 import (
-	"github.com/rs/zerolog/log"
 	"image"
 	"math"
 )
 
 // getMapViewportFromCoordinate calculates which part of the map should be visible inside the viewport.
 func getMapViewportFromCoordinate(screenWidth int, screenHeight int, coordinate Coordinate) *image.Rectangle {
-	log.Printf("getMapViewportFromCoordinate: screenWidth: %d, screenHeight: %d, coordinate: %v",
-		screenWidth, screenHeight, coordinate)
+	//log.Printf("getMapViewportFromCoordinate: screenWidth: %d, screenHeight: %d, coordinate: %v",
+	//	screenWidth, screenHeight, coordinate)
 	// TODO: We need some handling if coordinate is near the edge of the worldmap. We dont want to show white void.
 	windowWidthInCoords := int(math.Ceil(float64(screenWidth) / TileSize))
 	windowHeightInCoords := int(math.Ceil(float64(screenHeight) / TileSize))
 
-	// -1 and +1 below is there to show tiles when the user scrolls the map, i.e. when the camera changes position. This
+	// extraTiles below is there to show tiles when the user scrolls the map, i.e. when the camera changes position. This
 	// makes the camera show the next tile.
-	xCoordsMin := int(math.Floor(float64(coordinate.X)-float64(windowWidthInCoords)/2)) - 1
-	yCoordsMin := int(math.Floor(float64(coordinate.Y)-float64(windowHeightInCoords/2))) - 1
+	xCoordsMin := int(math.Floor(float64(coordinate.X)-float64(windowWidthInCoords)/2)) - extraTiles
+	yCoordsMin := int(math.Floor(float64(coordinate.Y)-float64(windowHeightInCoords/2))) - extraTiles
 
-	xCoordsMax := int(math.Ceil(float64(xCoordsMin+windowWidthInCoords))) + 1
-	yCoordsMax := int(math.Ceil(float64(yCoordsMin+windowHeightInCoords))) + 1
+	xCoordsMax := int(math.Ceil(float64(xCoordsMin+windowWidthInCoords))) + extraTiles
+	yCoordsMax := int(math.Ceil(float64(yCoordsMin+windowHeightInCoords))) + extraTiles
 
 	viewPortCoords := image.Rect(
 		xCoordsMin,
@@ -32,3 +31,5 @@ func getMapViewportFromCoordinate(screenWidth int, screenHeight int, coordinate 
 	r := viewPortCoords
 	return &r
 }
+
+const extraTiles = 0
